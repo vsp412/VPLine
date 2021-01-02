86e0bd7ec7e250aa04adc09bab950986acd749ea
Vineet Pande <vineet@Vineets-MacBook-Air.local>
2021-01-01 20:04:27 -0800

    initial commit

---
+++ VPLine/src/main/java/com/vinx/vpline/mLine.java
@@ -0,0 +1,11 @@
+package com.vinx.vpline;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class mLine {
+
+    public static void test(Context c, String message){
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
